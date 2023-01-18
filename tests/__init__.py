a07f07c2235099c6a234b5d5c2a45c753c593046
cmungall <cjm@berkeleybop.org>
2023-01-17 17:32:05 -0800

    add missing

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import os
+import pprint
+
+RESOURCE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "input")
+TARGET_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "target")
+JSONLD_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../jsonld")
